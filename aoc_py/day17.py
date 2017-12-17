479b43e3746cbe965dde035d6f7e75bbc07a50f9
Frazer McLean <frazer@frazermclean.co.uk>
2017-12-17 23:44:32 +0100

    day 17

---
+++ aoc_py/day17.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def spinlock(steps, loops, after):
+    buffer = deque([0])
+    position = 0
+
+    for i in range(1, loops + 1):
+        position += steps
+        position %= len(buffer)
+
+        buffer.insert(position + 1, i)
+
+        position += 1
+
+    return buffer[buffer.index(2017) + 1]
+
+
+def spinlock_zero(steps, loops):
+    position = 0
+    after0 = None
+
+    for i in range(1, loops + 1):
+        position += steps
+        position %= i  # i == buffer length
+
+        if position == 0:
+            after0 = i
+
+        position += 1
+
+    return after0
+
+
+if __name__ == '__main__':
+    steps = 335
+    print('Part 1:', spinlock(steps, loops=2017, after=2017))
+    print('Part 2:', spinlock_zero(steps, loops=int(50e6)))
+
+
